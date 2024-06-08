2921c5036813e82eaf75246d833bd28c42851a7f
Andrés Martínez <andres.martinez@proveeduria.unam.mx>
2024-06-08 10:26:49 -0600

    Initial Commit

---
+++ Internet-SUI/Internet_SUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Internet_SUIApp.swift
+//  Internet-SUI
+//
+//  Created by DISMOV on 08/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Internet_SUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
